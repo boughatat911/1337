38631c9783ac9e80c4d4c330593c1e4c33fe599f
root <root@BTCMETA.localdomain>
2024-08-18 20:21:50 +0100

    0.1

---
+++ Libtf/5-ft_isprint.c
@@ -0,0 +1,9 @@
+int ft_isprint(int c)
+{
+	if ((c >= 0 && c <= 31) || (c == 127))
+	{
+		return (0);
+	}
+	else 
+	return (1);
+}
---
+++ Libtf/6-ft_strlen.c
@@ -0,0 +1,10 @@
+int ft_strlen(char *s)
+{
+	int c;
+	c = 0;
+	while (s[c])
+	{
+		c++;
+	}
+	return (c);
+}
